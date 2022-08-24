a70710be0b378d3c31718285692b90781745b6b4
TheresiaRumapea <mgatheresia@gmail.com>
2022-08-24 17:31:21 +0700

    Tugas Repository

---
+++ Tes.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Tes {
+
+       /**
+        * @param args
+        */
+       public static void main(String[] args) {
+              // TODO Auto-generated method stub
+            
+              String nama;
+            
+              Scanner input_program = new Scanner (System.in);
+              System.out.print("Masukan Nama Anda: ");
+              nama = input_program.nextLine();
+              System.out.println("Nama Kamu : " + nama);    
+       }
+
+}
